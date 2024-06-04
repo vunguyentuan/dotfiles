45e6398fa49a7f3f28b9e01031bd5c5641ba938b
Vu Nguyen <tuanvu.vn007@gmail.com>
2024-06-04 12:00:34 +0700

    add zen mode

--- .config/nvim/lua/plugins/zenmode.lua
+++ .config/nvim/lua/plugins/zenmode.lua
@@ -1,5 +1,14 @@
 return {
   "folke/zen-mode.nvim",
+  keys = {
+    {
+      "<leader>z",
+      function()
+        require("zen-mode").toggle()
+      end,
+      desc = "Zen Mode",
+    },
+  },
   opts = {
     -- your configuration comes here
     -- or leave it empty to use the default settings
