283eb7448526100e8fc2dbf447a0b0f6dd8880be
Vu Nguyen <tuanvu.vn007@gmail.com>
2024-11-11 10:58:58 +0800

    chore: not showing untrack files

--- .hammerspoon/init.lua
+++ .hammerspoon/init.lua
@@ -3,8 +3,9 @@ local hotkey = require("hs.hotkey")
 
 -- Bind shortcuts to applications
 hotkey.bind({"shift", "alt", "ctrl"}, "g", function() hs.application.launchOrFocus("Figma") end)
-hotkey.bind({"shift", "alt", "ctrl"}, "f", function() hs.application.launchOrFocus("Wezterm") end)
-hotkey.bind({"shift", "alt", "ctrl"}, "d", function() hs.application.launchOrFocus("Arc") end)
+hotkey.bind({"shift", "alt", "ctrl"}, "f", function() hs.application.launchOrFocus("Ghostty") end)
+-- hotkey.bind({"shift", "alt", "ctrl"}, "f", function() hs.application.launchOrFocus("Kitty") end)
+hotkey.bind({"shift", "alt", "ctrl"}, "d", function() hs.application.launchOrFocus("Google Chrome") end)
 hotkey.bind({"shift", "alt", "ctrl"}, "s", function() hs.application.launchOrFocus("Craft") end)
 hotkey.bind({"shift", "alt", "ctrl"}, "a", function() hs.application.launchOrFocus("Slack") end)
 
