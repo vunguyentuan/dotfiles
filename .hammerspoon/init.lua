095caf01d6bd1a6f3b646b150861c6b0aa65109c
Vu Nguyen <tuanvu.vn007@gmail.com>
2024-12-11 11:08:41 +0800

    chore: change default browser to wezterm again

--- .hammerspoon/init.lua
+++ .hammerspoon/init.lua
@@ -3,7 +3,8 @@ local hotkey = require("hs.hotkey")
 
 -- Bind shortcuts to applications
 hotkey.bind({"shift", "alt", "ctrl"}, "g", function() hs.application.launchOrFocus("Figma") end)
-hotkey.bind({"shift", "alt", "ctrl"}, "f", function() hs.application.launchOrFocus("Ghostty") end)
+hotkey.bind({"shift", "alt", "ctrl"}, "f", function() hs.application.launchOrFocus("Wezterm") end)
+-- hotkey.bind({"shift", "alt", "ctrl"}, "f", function() hs.application.launchOrFocus("Ghostty") end)
 -- hotkey.bind({"shift", "alt", "ctrl"}, "f", function() hs.application.launchOrFocus("Kitty") end)
 hotkey.bind({"shift", "alt", "ctrl"}, "d", function() hs.application.launchOrFocus("Google Chrome") end)
 hotkey.bind({"shift", "alt", "ctrl"}, "s", function() hs.application.launchOrFocus("Craft") end)
